d628ef9560adcef8229c33f6addc49314cedf576
Alexander.Escobar <alexander.enrique.escobar@gmail.com>
2025-05-12 23:20:20 -0600

    Primer Commit desde local

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # MinervaJS Audit Logs
 Módulo de gestión de mensajes de consola para su uso en auditoria, depuración, informativos, avisos y error.
+
+
+![Descripción de la imagen](/images/CapturaAuditLogs.PNG)
--- src/xapp.js
+++ src/xapp.js
@@ -1,4 +1,4 @@
-/** @module Audit_Logs */
+/** @module Logger */
 const fs = require('node:fs/promises');
 const path = require('node:path');
 
@@ -6,21 +6,21 @@ const path = require('node:path');
 const colores = 
 {
   reset: '\x1b[0m',
-  rojo: '\x1b[31m',			//
-  verde: '\x1b[32m',		//
-  amarillo: '\x1b[33m',		//
-  azul: '\x1b[34m',			//
+  rojo: '\x1b[31m',
+  verde: '\x1b[32m',
+  amarillo: '\x1b[33m',
+  azul: '\x1b[34m',
   magenta: '\x1b[35m',
   cian: '\x1b[36m',
   gris: '\x1b[90m',
 };
 
 /**
- * Clase Audit_Logs personalizada que permite registrar mensajes en la consola con colores
+ * Clase Logger personalizada que permite registrar mensajes en la consola con colores
  * por nivel y también guardarlos en archivos de log rotativos basados en el número de líneas.
  */
-class Audit_Logs 
-{
+ class Logger 
+ {
   /**
    * Crea una instancia del Logger.
    * @param {string} [prefijo=''] - Un prefijo opcional que se añadirá a cada mensaje de log.
@@ -28,9 +28,8 @@ class Audit_Logs
    * añadirá un sufijo numérico para la rotación.
    * @param {boolean} [useColors=true] - Indica si se deben usar colores en la salida de la consola.
    * @param {number} [maxLines=10000] - El número máximo de líneas por archivo de log antes de la rotación.
-   * @param {boolean} [writeToDisk=false] - Indica si se realizaran escrituras a disco en paralelo despues de la escritura en consola.
    */
-  constructor(prefijo = '', filenameBase = 'app.log', useColors = true, maxLines = 10000, writeToDisk = false) 
+  constructor(prefijo = '', filenameBase = 'app.log', useColors = true, maxLines = 10000) 
   {
     /**
      * El prefijo que se añade a cada mensaje de log.
@@ -53,11 +52,6 @@ class Audit_Logs
      */
     this.maxLines = maxLines;
     /**
-     * Indica si se realizaran escrituras a disco en paralelo despues de la escritura en consola.
-     * @type {boolean}
-     */
-    this.writeToDisk = writeToDisk;
-    /**
      * El número de línea actual en el archivo de log actual.
      * @private
      * @type {number}
@@ -75,12 +69,6 @@ class Audit_Logs
      * @type {string}
      */
     this.currentFilename = this.getFilename();
-    /**
-     *  Cola de promesas para asegurar el orden de escritura
-     * @private
-     * @type {Promise}
-     */	
-    this.writeQueue = Promise.resolve();
   }
 
   /**
@@ -96,17 +84,17 @@ class Audit_Logs
 
   /**
    * Rota el archivo de log actual, cerrándolo y creando uno nuevo con la siguiente secuencia.
-   * No necesitamos explícitamente cerrar el archivo con appendFile; el sistema operativo se encarga.
-   * La próxima escritura a un archivo con un nombre diferente creará uno nuevo.
    * @async
    * @private
    */
   async rotateLogFile() 
   {
-	let l_message;
     this.currentLineCount = 0;
     this.fileSequence++;
     this.currentFilename = this.getFilename();
+    console.log(`Rotando archivo de log a: ${this.currentFilename}`);
+    // No necesitamos explícitamente cerrar el archivo con appendFile; el sistema operativo se encarga.
+    // La próxima escritura a un archivo con un nombre diferente creará uno nuevo.
   }
 
   /**
@@ -115,83 +103,90 @@ class Audit_Logs
    * @param {string} nivel - El nivel del log ('info', 'debug', 'warn', 'error').
    * @param {string} mensaje - El mensaje que se va a registrar.
    */
-  log(nivel, mensaje)
+  async log(nivel, mensaje) 
   {
     const ahora = new Date().toISOString();
     const nivelTexto = nivel.toUpperCase();
-    let lineaConsola = `[${ahora}] [${this.prefijo}]`;
+    let lineaConsola = `[${ahora}] [${this.prefijo}] [${nivelTexto}] ${mensaje}`;
     const lineaArchivo = `[${ahora}] [${this.prefijo}] [${nivelTexto}] ${mensaje}\n`;
 
-	// Aplicar colores a la salida de la consola
+    // Aplicar colores a la salida de la consola
     if (this.useColors) 
 	{
       switch (nivel) 
 	  {
-        case 'info': lineaConsola 	= `${lineaConsola} ${colores.verde}[${nivelTexto}] ${mensaje}${colores.reset}`; break;
-        case 'debug': lineaConsola 	= `${lineaConsola} ${colores.azul}[${nivelTexto}] ${mensaje}${colores.reset}`; break;
-        case 'warn': lineaConsola 	= `${lineaConsola} ${colores.amarillo}[${nivelTexto}] ${mensaje}${colores.reset}`; break;
-        case 'error': lineaConsola 	= `${lineaConsola} ${colores.rojo}[${nivelTexto}] ${mensaje}${colores.reset}`; break;
+        case 'info':
+          lineaConsola = `${colores.verde}${lineaConsola}${colores.reset}`;
+          break;
+        case 'debug':
+          lineaConsola = `${colores.azul}${lineaConsola}${colores.reset}`;
+          break;
+        case 'warn':
+          lineaConsola = `${colores.amarillo}${lineaConsola}${colores.reset}`;
+          break;
+        case 'error':
+          lineaConsola = `${colores.rojo}${lineaConsola}${colores.reset}`;
+          break;
+        default:
+          break;
       }
     }
 
-	// Mostrar en consola
+    // Mostrar en consola
     console.log(lineaConsola);
 
-	// Escribir en archivo
-	// Si writeToDisk es false, simplemente omitimos la escritura.
-    if (this.writeToDisk) 
+    // Escribir en archivo de forma asíncrona
+    try 
 	{
-	  // Encolar la operación de escritura
-      this.writeQueue = this.writeQueue.then(async () => {
-        try 
-		{
-          this.currentLineCount++;
-          if (this.currentLineCount >= this.maxLines) 
-		  { await fs.appendFile(this.currentFilename, lineaArchivo + `Rotando archivo de log ${this.currentFilename}\r`, 'utf8');
-			await this.rotateLogFile(); }
-		  else
-		  { await fs.appendFile(this.currentFilename, lineaArchivo, 'utf8'); }
-        } 
-		catch (error) 
-		{
-          console.error('Error al escribir en el archivo de log:', error);
-          return Promise.resolve();
-        }
-      });
-    }
+	  console.log("this.currentLineCount");
+	  console.log(this.currentLineCount);
+      await fs.appendFile(this.currentFilename, lineaArchivo, 'utf8');
+      this.currentLineCount = this.currentLineCount + 1;
+      if (this.currentLineCount >= this.maxLines)
+	  { await this.rotateLogFile(); }
+    } 
+	catch (error) 
+	{ console.error('Error al escribir en el archivo de log:', error); }
   }
 
   /**
    * Registra un mensaje con nivel 'info'.
    * @param {string} mensaje - El mensaje informativo.
    */
-  info(mensaje) { this.log('info', mensaje); }
-  
+  info(mensaje) 
+  { this.log('info', mensaje); }
+
   /**
    * Registra un mensaje con nivel 'debug'.
    * @param {string} mensaje - El mensaje de depuración.
    */
-  debug(mensaje) { this.log('debug', mensaje); }
-  
+  debug(mensaje) 
+  { this.log('debug', mensaje); }
+
   /**
    * Registra un mensaje con nivel 'error'.
    * @param {string} mensaje - El mensaje de error.
    */
-  error(mensaje) { this.log('error', mensaje); }
-  
+  error(mensaje) 
+  { this.log('error', mensaje); }
+
   /**
    * Registra un mensaje con nivel 'warn'.
    * @param {string} mensaje - El mensaje de advertencia.
    */
-  warn(mensaje) { this.log('warn', mensaje); }
+  warn(mensaje) 
+  { this.log('warn', mensaje); }
 }
+
 
 let instance = null;
 
 module.exports = {
-  getInstance: (prefijo, filenameBase, useColors, maxLines, writeToDisk) => {
-    if (!instance) 
-	{ instance = new Audit_Logs(prefijo, filenameBase, useColors, maxLines, writeToDisk); }
-    return instance;
-  },
+    getInstance: (prefijo, filenameBase, useColors, maxLines) => {
+        if (!instance) 
+		{ instance = new Logger(prefijo, filenameBase, useColors, maxLines); }
+        return instance;
+    },
 };
+
+// module.exports = Logger; // Exportamos la clase para poder usarla en otros módulos
